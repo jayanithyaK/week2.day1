78b3f6698abf23e84837ce7da95c26e063c3e1e1
jayanithyaK <101966224+jayanithyaK@users.noreply.github.com>
2022-03-27 18:53:59 +0530

    Add files via upload

---
+++ Edit.java
@@ -0,0 +1,61 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class Edit {
+	public static void main(String[] args) {
+	
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("abcd");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("jayanithya");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("K");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("nithya");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("IT");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("this is testleaf assignment");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("nithya301192@gmail.com");
+		
+		
+	 WebElement eledropdown = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		
+		Select dd = new Select(eledropdown);
+		
+		dd.selectByVisibleText("New York");
+		
+		driver.findElement(By.className("smallSubmit")).click();
+		
+		driver.findElement(By.linkText("Edit")).click();
+		
+		driver.findElement(By.id("updateLeadForm_description")).clear();
+		
+		driver.findElement(By.id("pdateLeadForm_importantNote")).sendKeys("assdsjfheiucuhfd");
+		
+		driver.findElement(By.className("smallSubmit")).click();
+		
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+
+	}
+
+}
+	
+	
+	
+	
+	
+
